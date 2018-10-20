d33648b913e564e5b4d722ba6734cb6ca77b6d5f
Jefferson Cabral <jeffersoncabral_10@hotmail.com>
2018-10-20 12:26:44 -0300

    adicona arquivos da nave

---
+++ Assets/Scripts/Atirar.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Atirar : MonoBehaviour {
+	public GameObject tiro;
+	public float frequenciaDisparo = 0.5f;
+	private float proximoDisparo = 0;
+
+	void Start () {
+		
+	}
+	
+
+	void Update ()
+	{
+		if (Input.GetButton ("Fire1") && Time.time >= proximoDisparo) {
+			proximoDisparo = Time.time + frequenciaDisparo;
+			Instantiate (tiro, transform);
+		}
+	}
+}
---
+++ Assets/Scripts/rotacionarAleatorio.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotacionarAleatorio : MonoBehaviour {
+
+	private Rigidbody componenteRb;
+	public int forca = 50;
+	void Start () {
+		componenteRb = GetComponent<Rigidbody> ();
+		componenteRb.AddTorque (Random.insideUnitCircle * forca);
+	}
+		
+}
